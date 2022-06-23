9a77e37ffdcb898be5409a5dd532459db058c649
Juan Ramón Then B <juanramon1123@hotmail.com>
2022-06-23 09:33:59 -0400

    Loading.gif centered

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,12 +4,18 @@ import spin from './Loading.gif';
 const Loading = () => {
   return (
     <Fragment>
-      <img
-        src={spin}
-        alt='Loading...'
-        className='inline-block align-middle'
-        style={{ width: '200px', margin: 'auto' }}
-      />
+      <div className='container'>
+        <div className='row'>
+          <div className='col col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-12 text-center'>
+            <img
+              src={spin}
+              alt='Loading...'
+              className='inline-block align-middle'
+              style={{ width: '200px', margin: 'auto' }}
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
