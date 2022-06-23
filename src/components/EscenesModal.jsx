4cdfec0c70b33853ce46c4a6ac326c6a9accc124
Juan Ramón Then B <juanramon1123@hotmail.com>
2022-06-23 10:29:22 -0400

    Left panel buttoms UI

--- src/components/EscenesModal.jsx
+++ src/components/EscenesModal.jsx
@@ -1,42 +1,42 @@
 const EscenesModal = () => {
   return (
     <div
-      class="modal fade"
-      id="escenarios"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
+      class='modal fade'
+      id='escenarios'
+      tabindex='-1'
+      aria-labelledby='exampleModalLabel'
+      aria-hidden='true'
     >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header bg-primary">
-            <h5 class="modal-title text-white" id="exampleModalLabel">
+      <div class='modal-dialog'>
+        <div class='modal-content'>
+          <div class='modal-header bg-primary'>
+            <h5 class='modal-title text-white' id='exampleModalLabel'>
               Crear/cargar escenarios...
             </h5>
             <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
+              type='button'
+              class='btn-close'
+              data-bs-dismiss='modal'
+              aria-label='Close'
             ></button>
           </div>
-          <div class="modal-body">
-            <select class="form-select" aria-label="Default select example">
-              <option selected value="1">
+          <div class='modal-body'>
+            <select class='form-select' aria-label='Default select example'>
+              <option selected value='1'>
                 Crear escenario
               </option>
-              <option value="2">Seleccionar escenario</option>
+              <option value='2'>Seleccionar escenario</option>
             </select>
           </div>
-          <div class="modal-footer">
+          <div class='modal-footer'>
             <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
+              type='button'
+              class='btn btn-secondary'
+              data-bs-dismiss='modal'
             >
               Close
             </button>
-            <button type="button" class="btn btn-primary">
+            <button type='button' class='btn btn-primary'>
               Save changes
             </button>
           </div>
--- src/components/ExportButtons.jsx
+++ src/components/ExportButtons.jsx
@@ -1,12 +1,15 @@
 const ExportButtons = () => {
   return (
     <div className='button-export-wrapper'>
-      <button className='btn btn-success btn-sm shadow' type='button'>
+      <button
+        className='btn btn-success btn-sm shadow w-100 text-start'
+        type='button'
+      >
         <i class='fa-solid fa-file-excel excel-icon'> Exportar reporte</i>
       </button>
       <button
         type='button'
-        class='btn btn-success btn-sm shadow'
+        class='btn btn-success btn-sm shadow w-100 my-1 text-start'
         data-togglebs-toggle='modal'
         data-bs-target='#cuadrante'
       >
@@ -15,7 +18,10 @@ const ExportButtons = () => {
           Importar parametros
         </i>
       </button>
-      <button className='btn btn-success btn-sm shadow' type='button'>
+      <button
+        className='btn btn-success btn-sm shadow w-100 text-start'
+        type='button'
+      >
         <i class='fa-solid fa-file-excel excel-icon'> Exportar paramametros</i>
       </button>
     </div>
