7a357359a750c226865584409647791dabc8d361
Juan Ramón Then B <juanramon1123@hotmail.com>
2022-06-24 11:22:56 -0400

    Loading gif added to component and some minimal layout improvements

--- src/components/Escenestable.jsx
+++ src/components/Escenestable.jsx
@@ -1,41 +1,47 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loading from './Loading';
 const Escenestable = () => {
-  const campUrl = "http://localhost:8081/escenarios";
+  const campUrl = 'http://localhost:8081/escenarios';
   const [data, setData] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const res = await axios.get(campUrl);
       setData(res.data);
+      setLoading(false);
     };
     fetchData();
   }, []);
 
-  return (
-    <div className="table-responsive">
-      <table className="table align-middle table-striped table-hover">
-        <thead className="table-title">
-          <tr>
-            <th>Nombre</th>
-            <th>Descripcion</th>
-            <th>Ultima ejecucion</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {data.map((stages) => (
-            <tr key={stages.id}>
-              <td>{stages.Name}</td>
-              <td>{stages.U_Descr}</td>
-              <td>{stages.U_LastExecDate}</td>
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <div className='table-responsive'>
+        <table className='table align-middle table-striped table-hover'>
+          <thead className='table-title'>
+            <tr>
+              <th>Nombre</th>
+              <th>Descripcion</th>
+              <th>Ultima ejecucion</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+          </thead>
+
+          <tbody>
+            {data.map((stages) => (
+              <tr key={stages.id}>
+                <td>{stages.Name}</td>
+                <td>{stages.U_Descr}</td>
+                <td>{stages.U_LastExecDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
 
 export default Escenestable;
