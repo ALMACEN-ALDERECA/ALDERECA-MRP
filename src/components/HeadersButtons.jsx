0ad18ae4af64154bac1cbf33fee188b28da3df96
LeudysOD <leudis3@hotmail.com>
2022-06-27 17:03:07 -0400

    Other option in the nav added

--- src/components/HeadersButtons.jsx
+++ src/components/HeadersButtons.jsx
@@ -1,45 +1,53 @@
 const HeadersButtons = () => {
   return (
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-      <div class='container-fluid'>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
         {/* <a class='navbar-brand' href='#'>
           Navbar
         </a> */}
         <button
-          class='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarNav'
-          aria-controls='navbarNav'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <span class='navbar-toggler-icon'></span>
+          <span class="navbar-toggler-icon"></span>
         </button>
         <div
-          class='collapse navbar-collapse justify-content-between'
-          id='navbarNav'
+          class="collapse navbar-collapse justify-content-between"
+          id="navbarNav"
         >
-          <ul class='navbar-nav'>
-            <li class='nav-item'>
+          <ul class="navbar-nav">
+            <li class="nav-item">
               <button
-                className='border-0 bg-white'
-                type='button'
-                data-bs-toggle='offcanvas'
-                data-bs-target='#offcanvasScrolling'
-                aria-controls='offcanvasScrolling'
+                className="border-0 bg-white"
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasScrolling"
+                aria-controls="offcanvasScrolling"
               >
                 Opciones
               </button>
+              <button
+                type="button"
+                class="btn btn-light   ms-2 btn-xs"
+                data-toggle="modal"
+                data-target="#modelId"
+              >
+                Analisis por mes
+              </button>
             </li>
           </ul>
-          <li class='nav-item list-group-numbered'>
+          <li class="nav-item list-group-numbered">
             <input
-              class='form-check-input shadow'
-              type='checkbox'
-              id='flexSwitchCheckDefault'
+              class="form-check-input shadow"
+              type="checkbox"
+              id="flexSwitchCheckDefault"
             />
-            <i class='fa-solid fa-chart-line ps-2 pt-1 me-1'></i>
+            <i class="fa-solid fa-chart-line ps-2 pt-1 me-1"></i>
           </li>
         </div>
       </div>
