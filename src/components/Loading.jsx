e0c5efc231307496beb119a3ffaaa8052d8f1b21
Prisila Everly Baez Mendez <pbaez@aldereca.com>
2022-06-29 13:55:36 -0400

    little changes (navbar bottom line disable and shadows)

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -11,7 +11,7 @@ const Loading = () => {
               src={spin}
               alt='Loading...'
               className='inline-block align-middle'
-              style={{ width: '200px', margin: '13% auto' }}
+              style={{ width: '200px', margin: '14% auto' }}
             />
           </div>
         </div>
